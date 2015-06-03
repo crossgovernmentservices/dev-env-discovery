b4160695b33abeaa386dc9b029511a4b70e21024
Juan Matthys Uys <opyate@gmail.com>
2015-06-03 11:01:31 +0100

    db script to support maslow too

--- script/db.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "password is: prototypes"
-export HASH=$(sudo docker ps -a|grep "devenv_db"|cut -f1 -d' ')
-psql -h $(sudo docker inspect $HASH | jq -r '.[].NetworkSettings.IPAddress') prototypes -p 5432 -U prototypes
