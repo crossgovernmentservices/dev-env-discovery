ce61b7ebf483a8f0b3648c30c397a4765e0ff6ab
Juan Matthys Uys <opyate@gmail.com>
2015-06-08 20:32:12 +0100

    python script to generate docker composes configs in favour of bash script

---
+++ compose.py
@@ -0,0 +1,64 @@
+import yaml
+import sys
+import os
+import re
+
+
+TEMPLATE='docker-compose-template.yml'
+SECRETS='~/.xgsenv'
+
+def write_config(template, filename):
+    f = open(filename, 'w')
+    yaml.dump(template, f)
+    f.close()
+
+def get_image(app):
+    if bool(re.match('apps/\w+', app)):
+        return 'tutum.co/govuk/%s' % app.split('/')[1]
+
+
+def main():
+    print('Generate config')
+    which = raw_input('1: dev\n2: aws\n\n')
+    if which == '1':
+        which = 'dev'
+    elif which == '2':
+        which = 'aws' 
+    else:
+        print('Bad option. Exiting...')
+        sys.exit()
+
+    with open(TEMPLATE) as template:
+       template_yaml = template.read()
+    with open(os.path.expanduser(SECRETS)) as secrets:
+        secrets_yaml = secrets.read()
+    secrets = yaml.load(secrets_yaml)
+
+    # replace secrets
+    for k,v in secrets.iteritems():
+        key = '__%s__' % k
+        template_yaml = template_yaml.replace(key, str(v))
+
+    template = yaml.load(template_yaml)
+
+    if which == 'dev':
+        write_config(template, 'docker-compose.yml')
+    elif which == 'aws':
+        del template['maslow']
+        del template['postgres']
+        # replace 'build' with 'image'
+        for app, nested in template.iteritems():
+            for key, val in nested.iteritems():
+                if key == 'build':
+                    del nested[key]
+                    nested['image'] = get_image(val)
+                    template[app] = nested
+                    break
+            else:
+                continue
+
+        write_config(template, 'aws.yml')
+
+
+if __name__ == '__main__':
+    main()
--- prep.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf docker-compose.yml
-cp docker-compose-template.yml docker-compose.yml
-while read CONF_ITEM; do
-  export CONF_ITEM_KEY=$(echo $CONF_ITEM | cut -f1 -d':')
-  export CONF_ITEM_VALUE=$(echo $CONF_ITEM | cut -f2 -d':')
-  perl -pi -e 's#%$ENV{CONF_ITEM_KEY}%#$ENV{CONF_ITEM_VALUE}#g' docker-compose.yml
-done < ~/.xgsenv
